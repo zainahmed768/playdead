764f8ec02d24df0daf46b9b5b5d1f11607a5ead8
Waleed Ghani <waleed.ghani@inoviotech.local>
2023-01-12 00:11:28 +0500

    Home page created new

---
+++ home.php
@@ -0,0 +1,451 @@
+<?php include 'inc/app.php' ?>
+<!-- css start here -->
+<link rel="stylesheet" href="css/home.css">
+<link rel="stylesheet" href="css/auction.css">
+<!-- css start here -->
+
+<section class="fram-sec">
+    <div class="frame-wrapper">
+        <div class="border-left">
+            <img src="img/borderleft.png" class="img-fluid" alt="">
+        </div>
+        <div class="centerLogoDv">
+            <figure><img src="img/logodv.png" alt=""></figure>
+        </div>
+        <div class="border-right">
+            <img src="img/borderright.png" class="img-fluid" alt="">
+        </div>
+        <!-- LOGO -->
+        <div class="TopCenter">
+            <?php include 'inc/logo.php'; ?>
+        </div>
+        <!-- LOGO -->
+
+        <!-- Menus -->
+        <div class="row">
+            <!-- <div class="col-md-7">
+
+            </div> -->
+            <div class="col-md-12">
+                <div class="Menus text-right">
+                    <?php include 'inc/menu.php'; ?>
+                </div>
+            </div>
+        </div>
+        <!-- Menus -->
+
+        <!-- Home Sec Start Here -->
+        <section class="home_sec">
+            <div class="all-images">
+                <div class="img-box">
+                    <div class="picture-box">
+                        <figure><img src="img/banner1.png" class="img-fluid" alt=""></figure>
+                    </div>
+                    <div class="txt-box">
+                        <figure><img src="img/banner-heading1.png" alt="" class="img-fluid"></figure>
+                        <div class="button-group">
+                            <a href="#" class="btn">Play</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="img-box">
+                    <div class="picture-box">
+                        <figure><img src="img/banner2.png" class="img-fluid" alt=""></figure>
+                    </div>
+                    <div class="txt-box">
+                        <figure><img src="img/banner-heading2.png" alt="" class="img-fluid"></figure>
+                        <div class="button-group">
+                            <a href="#" class="btn">Play</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="img-box">
+                    <div class="picture-box">
+                        <figure><img src="img/banner3.png" class="img-fluid" alt=""></figure>
+                    </div>
+                    <div class="txt-box">
+                        <figure><img src="img/banner-heading3.png" alt="" class="img-fluid"></figure>
+                        <div class="button-group">
+                            <a href="#" class="btn">Play</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="img-box">
+                    <div class="picture-box">
+                        <figure><img src="img/banner4.png" class="img-fluid" alt=""></figure>
+                    </div>
+                    <div class="txt-box">
+                        <figure><img src="img/banner-heading4.png" alt="" class="img-fluid"></figure>
+                        <div class="button-group">
+                            <a href="#" class="btn">Play</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="img-box">
+                    <div class="picture-box">
+                        <figure><img src="img/banner5.png" class="img-fluid" alt=""></figure>
+                    </div>
+                    <div class="txt-box">
+                        <figure><img src="img/banner-heading5.png" alt="" class="img-fluid"></figure>
+                        <div class="button-group">
+                            <a href="#" class="btn">Play</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="img-box">
+                    <div class="picture-box">
+                        <figure><img src="img/banner6.png" class="img-fluid" alt=""></figure>
+                    </div>
+                    <div class="txt-box">
+                        <figure><img src="img/banner-heading6.png" alt="" class="img-fluid"></figure>
+                        <div class="button-group">
+                            <a href="#" class="btn">Play</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Home Sec End Here -->
+
+    </div>
+</section>
+
+<!-- Play Game Sec Start Here -->
+<section class="play_game">
+    <div class="play-game-box">
+        <div class="img-box">
+            <figure><img src="img/play-game.png" alt="" class="img-fluid"></figure>
+        </div>
+        <div class="button-group">
+            <a href="#" class="play-btn">Play Now</a>
+        </div>
+    </div>
+</section>
+<!-- Play Game Sec End Here -->
+
+<!-- Auction Sec Start Here -->
+<section class="auction_sec">
+    <div class="container">
+        <div class="heading-wrapper text-center py-4">
+            <figure><img src="img/auction-head.png" alt="" class="img-fluid"></figure>
+        </div>
+
+        <ul class="auction-items">
+            <li>
+                <div class="auction-box">
+                    <div class="txt">
+                        <h4>xyz Awards</h4>
+                    </div>
+                    <div class="img-box">
+                        <figure><img src="img/auction1.png" alt="123" class="img-fluid"></figure>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="auction-box">
+                    <div class="txt">
+                        <h4>xyz Awards</h4>
+                    </div>
+                    <div class="img-box">
+                        <figure><img src="img/auction2.png" alt="123" class="img-fluid"></figure>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="auction-box">
+                    <div class="txt">
+                        <h4>xyz Awards</h4>
+                    </div>
+                    <div class="img-box">
+                        <figure><img src="img/auction1.png" alt="123" class="img-fluid"></figure>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="auction-box">
+                    <div class="txt">
+                        <h4>xyz Awards</h4>
+                    </div>
+                    <div class="img-box">
+                        <figure><img src="img/auction3.png" alt="123" class="img-fluid"></figure>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="auction-box middle-box">
+                    <div class="img-box">
+                        <figure><img src="img/auction2.png" alt="123" class="img-fluid"></figure>
+                    </div>
+                    <div class="txt">
+                        <h4>xyz Awards</h4>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="auction-box">
+                    <div class="txt">
+                        <h4>xyz Awards</h4>
+                    </div>
+                    <div class="img-box">
+                        <figure><img src="img/auction1.png" alt="123" class="img-fluid"></figure>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="auction-box">
+                    <div class="txt">
+                        <h4>xyz Awards</h4>
+                    </div>
+                    <div class="img-box">
+                        <figure><img src="img/auction3.png" alt="123" class="img-fluid"></figure>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="auction-box">
+                    <div class="txt">
+                        <h4>xyz Awards</h4>
+                    </div>
+                    <div class="img-box">
+                        <figure><img src="img/auction2.png" alt="123" class="img-fluid"></figure>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="auction-box">
+                    <div class="txt">
+                        <h4>xyz Awards</h4>
+                    </div>
+                    <div class="img-box">
+                        <figure><img src="img/auction3.png" alt="123" class="img-fluid"></figure>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
+<!-- Auction Sec End Here -->
+
+<!-- Pets Sec Start Here -->
+<section class="pets_sec">
+    <div class="heading">
+        <figure><img src="img/pets-head.png" alt="" class="img-fluid"></figure>
+    </div>
+    <div class="all-pets">
+        <div class="pet-box">
+            <div class="pet-img">
+                <figure><img src="img/pett1.png" alt="" class="img-fluid"></figure>
+            </div>
+            <div class="detail">
+                <div class="name">
+                    <h4 class="pet-name">Aldrich's</h4>
+                    <h4 class="pet-price">$10</h4>
+                </div>
+                <div class="button-group">
+                    <a href="#" class="buy-now-btn">Buy now</a>
+                </div>
+            </div>
+        </div>
+        <div class="pet-box">
+            <div class="pet-img">
+                <figure><img src="img/pett2.png" alt="" class="img-fluid"></figure>
+            </div>
+            <div class="detail">
+                <div class="name">
+                    <h4 class="pet-name">Aldrich's</h4>
+                    <h4 class="pet-price">$10</h4>
+                </div>
+                <div class="button-group">
+                    <a href="#" class="buy-now-btn">Buy now</a>
+                </div>
+            </div>
+        </div>
+        <div class="pet-box">
+            <div class="pet-img">
+                <figure><img src="img/pett3.png" alt="" class="img-fluid"></figure>
+            </div>
+            <div class="detail">
+                <div class="name">
+                    <h4 class="pet-name">Aldrich's</h4>
+                    <h4 class="pet-price">$10</h4>
+                </div>
+                <div class="button-group">
+                    <a href="#" class="buy-now-btn">Buy now</a>
+                </div>
+            </div>
+        </div>
+        <div class="pet-box">
+            <div class="pet-img">
+                <figure><img src="img/pett1.png" alt="" class="img-fluid"></figure>
+            </div>
+            <div class="detail">
+                <div class="name">
+                    <h4 class="pet-name">Aldrich's</h4>
+                    <h4 class="pet-price">$10</h4>
+                </div>
+                <div class="button-group">
+                    <a href="#" class="buy-now-btn">Buy now</a>
+                </div>
+            </div>
+        </div>
+        <div class="pet-box">
+            <div class="pet-img">
+                <figure><img src="img/pett2.png" alt="" class="img-fluid"></figure>
+            </div>
+            <div class="detail">
+                <div class="name">
+                    <h4 class="pet-name">Aldrich's</h4>
+                    <h4 class="pet-price">$10</h4>
+                </div>
+                <div class="button-group">
+                    <a href="#" class="buy-now-btn">Buy now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Pets Sec End Here -->
+
+<!-- Store Sec Start Here -->
+<section class="store_sec">
+    <div class="heading">
+        <h3>Store</h3>
+    </div>
+    <div class="all-items">
+        <div class="item-box">
+            <div class="item-img">
+                <figure><img src="img/black1.png" alt="" class="img-fluid"></figure>
+            </div>
+            <div class="item-detail">
+                <div class="name">
+                    <h4 class="item-name">mystic blade</h4>
+                    <h4 class="item-price">$10</h4>
+                </div>
+                <div class="button-group">
+                    <a href="#" class="buy-btn">Buy now</a>
+                </div>
+            </div>
+        </div>
+        <div class="item-box">
+            <div class="item-img">
+                <figure><img src="img/black2.png" alt="" class="img-fluid"></figure>
+            </div>
+            <div class="item-detail">
+                <div class="name">
+                    <h4 class="item-name">mystic blade</h4>
+                    <h4 class="item-price">$10</h4>
+                </div>
+                <div class="button-group">
+                    <a href="#" class="buy-btn">Buy now</a>
+                </div>
+            </div>
+        </div>
+        <div class="item-box">
+            <div class="item-img">
+                <figure><img src="img/black3.png" alt="" class="img-fluid"></figure>
+            </div>
+            <div class="item-detail">
+                <div class="name">
+                    <h4 class="item-name">mystic blade</h4>
+                    <h4 class="item-price">$10</h4>
+                </div>
+                <div class="button-group">
+                    <a href="#" class="buy-btn">Buy now</a>
+                </div>
+            </div>
+        </div>
+        <div class="item-box">
+            <div class="item-img">
+                <figure><img src="img/black4.png" alt="" class="img-fluid"></figure>
+            </div>
+            <div class="item-detail">
+                <div class="name">
+                    <h4 class="item-name">mystic blade</h4>
+                    <h4 class="item-price">$10</h4>
+                </div>
+                <div class="button-group">
+                    <a href="#" class="buy-btn">Buy now</a>
+                </div>
+            </div>
+        </div>
+        <div class="item-box">
+            <div class="item-img">
+                <figure><img src="img/black5.png" alt="" class="img-fluid"></figure>
+            </div>
+            <div class="item-detail">
+                <div class="name">
+                    <h4 class="item-name">mystic blade</h4>
+                    <h4 class="item-price">$10</h4>
+                </div>
+                <div class="button-group">
+                    <a href="#" class="buy-btn">Buy now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Store Sec End Here -->
+
+<!-- Archives Sec Start Here -->
+<section class="archives_sec">
+    <div class="heading">
+        <h3>The Archives -History Of Play Dead Live</h3>
+    </div>
+    <div class="history-scroll">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="img-box">
+                    <figure><img src="img/history1.png" alt="" class="img-fluid"></figure>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="content">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="content right">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="img-box right">
+                    <figure><img src="img/history1.png" alt="" class="img-fluid"></figure>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="img-box">
+                    <figure><img src="img/history1.png" alt="" class="img-fluid"></figure>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="content">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Archives Sec End Here -->
+
+<!-- Footer Start Here -->
+<?php include 'inc/footer.php' ?>
+<!-- Footer End Here -->
